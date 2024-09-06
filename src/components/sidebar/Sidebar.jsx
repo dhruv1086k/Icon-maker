7eb6952cc2c97af5d3bad2b75582d266e7b56919
dhruv <dhruvpal.2702@gmail.com>
2024-09-06 23:35:06 +0530

    add functionality to sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,8 +7,12 @@ import {
   Sun,
 } from "lucide-react";
 import styles from "./Sidebar.module.css";
+import { useState } from "react";
 
 export default function Sidebar() {
+  const [selected, setSelected] = useState(0);
+  const [themeColor, setThemeColor] = useState(0);
+
   const menu = [
     {
       name: "Icon",
@@ -23,6 +27,18 @@ export default function Sidebar() {
       icon: ShieldPlus,
     },
   ];
+
+  const themeList = [
+    {
+      name: "Dark",
+      icon: Moon,
+    },
+    {
+      name: "Light",
+      icon: Sun,
+    },
+  ];
+
   return (
     <>
       <div className={styles.container}>
@@ -31,21 +47,38 @@ export default function Sidebar() {
         </h1>
         <div className={styles.menu}>
           {menu.map((me, idx) => (
-            <div className={styles.menuChild}>
+            <div
+              onClick={() => {
+                setSelected(idx);
+              }}
+              key={idx}
+              className={styles.menuChild}
+              style={{
+                background: selected == idx && "#DF0025",
+                color: selected == idx && "#fff",
+              }}
+            >
               <me.icon />
               {me.name}
             </div>
           ))}
         </div>
         <div className={styles.theme}>
-          <div className={styles.themeChild}>
-            <Moon />
-            Dark
-          </div>
-          <div className={styles.themeChild}>
-            <Sun />
-            Light
-          </div>
+          {themeList.map((theme, idx) => (
+            <div
+              onClick={() => {
+                setThemeColor(idx);
+              }}
+              className={styles.themeChild}
+              style={{
+                background: themeColor == idx && "#DF0025",
+                color: themeColor == idx && "#fff",
+              }}
+            >
+              <theme.icon />
+              {theme.name}
+            </div>
+          ))}
         </div>
       </div>
     </>
